58522bc37b7e2cd99c39175be1e95b5addf6e23d
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-10 17:10:08 +0200

    Create artists section

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -58,4 +58,64 @@
 </section>
 <!-- /.comic_details -->
 
+<section class="artists">
+    <div class="container">
+        <div class="row">
+            <div class="col-2 left">
+                <div class="column_title">
+                    <h3>Talent</h3>
+                </div>
+                <!-- /.title -->
+                <div class="artists_list">
+                    <span>Art by:</span>
+
+                    @foreach($comic['artists'] as $artist)
+                    <span class="color_blue">{{$artist}},</span>
+                    @endforeach
+
+
+                </div>
+                <!-- /.art -->
+                <div class="writers_list">
+                    <span>Written by:</span>
+
+                    @foreach($comic['writers'] as $writer)
+                    <span class="color_blue">{{$writer}},</span>
+                    @endforeach
+
+                </div>
+                <!-- /.writers -->
+            </div>
+            <!-- /.col-2 -->
+
+            <div class="col-2 right">
+                <div class="column_title">
+                    <h3>Specs</h3>
+                </div>
+                <!-- /.title -->
+                <div class="series_name">
+                    <span>Series:</span>
+                    <span class="color_blue">{{$comic['series']}}</span>
+                </div>
+                <!-- /.series_name -->
+                <div class="price">
+                    <span>U.S. Price:</span>
+                    <span>{{$comic['price']}}</span>
+                </div>
+                <!-- /.price -->
+                <div class="sale_date">
+                    <span>U.S. Price:</span>
+                    <span>{{$comic['sale_date']}}</span>
+                </div>
+                <!-- /.sale_date -->
+            </div>
+            <!-- /.col-2 -->
+
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+</section>
+<!-- /.artists -->
+
 @endsection
